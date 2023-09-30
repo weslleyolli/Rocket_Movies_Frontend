a6d757584e52fca33b4800f66201d5b3adff514f
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-30 13:32:53 -0300

    test: starting project

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+export function SignIn() {
+  return (
+    <div>
+      <form>
+        <h1 className="">Rocket Movies</h1>
+        <p></p>
+      </form>
+    </div>
+  )
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,css}"],
+  theme: {
+    extend: {
+      colors: {
+        pinklight: "#FF859B",
+        graylight: "#CAC4CF",
+        graymedium: "#948F99",
+        white: "#F4EDE8",
+        inputColor: "#262529",
+        backgroundColor: "#1C1B1E",
+        baseMarker: "#0D0C0F",
+        notesBackgroundColor: "#312E38"
+      }
+    },
+  },
+  plugins: [],
+}
+
